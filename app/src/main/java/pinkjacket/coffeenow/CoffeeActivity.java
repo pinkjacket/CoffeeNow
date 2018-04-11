96866d8b596ba8ca6890ed207304c7b0f480077c
Student <example@example.com>
2018-04-11 11:03:57 -0700

    API call working

--- app/src/main/java/pinkjacket/coffeenow/CoffeeActivity.java
+++ app/src/main/java/pinkjacket/coffeenow/CoffeeActivity.java
@@ -3,13 +3,20 @@ package pinkjacket.coffeenow;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class CoffeeActivity extends AppCompatActivity {
     @BindView(R.id.zipView) TextView mZipView;
+    public static final String TAG = CoffeeActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,26 @@ public class CoffeeActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mZipView.setText("Zip code: " + location);
+        getCoffee(location);
+    }
+
+    private void getCoffee(String location){
+        final YelpService yelpService = new YelpService();
+        yelpService.findCoffee(location, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try{
+                    String jsonData = response.body().string();
+                    Log.v(TAG, jsonData);
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
